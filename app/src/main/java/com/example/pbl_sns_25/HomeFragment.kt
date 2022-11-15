70b8a6d90c79c52d489910ed1c9c1cd6129ddf3e
sujinyang <ysj9251@naver.com>
2022-11-15 13:35:47 +0900

    homefragment수정

--- app/src/main/java/com/example/pbl_sns_25/HomeFragment.kt
+++ app/src/main/java/com/example/pbl_sns_25/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,20 +17,36 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
+val db: FirebaseFirestore = Firebase.firestore
+val itemsCollectionRef = db.collection("items")
+
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private fun queryItem(itemID: String) {
+        itemsCollectionRef.document(itemID).get()
+            .addOnSuccessListener { // it: DocumentSnapshot
+                binding.editID.setText(it.id)
+                binding.editItemName.setText(it["name"].toString())
+                binding.editPrice.setText(it["price"].toString())
+        }.addOnFailureListener { }
     }
 
-    override fun onCreateView(
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }*/
+
+
+    }
+
+    /*override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -38,14 +55,14 @@ class HomeFragment : Fragment() {
     }
 
     companion object {
-        /**
+        *//**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
          * @return A new instance of fragment HomeFragment.
-         */
+         *//*
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
@@ -55,5 +72,5 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+    }*/
 }
