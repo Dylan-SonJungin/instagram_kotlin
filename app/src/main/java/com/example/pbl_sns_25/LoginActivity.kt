128d4e8910196619364cfb1d4b9c0a25638389eb
Jungin <jungin.dylan@gmail.com>
2022-11-15 13:03:46 +0900

    build.gradle-firebase 로그인

--- app/src/main/java/com/example/pbl_sns_25/LoginActivity.kt
+++ app/src/main/java/com/example/pbl_sns_25/LoginActivity.kt
@@ -1,4 +1,37 @@
 package com.example.pbl_sns_25
 
-class LoginActivity{
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pbl_sns_25.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.signIn.setOnClickListener {
+            val userEmail = binding.userEmail.text.toString()
+            val password = binding.password.text.toString()
+            doLogin(userEmail, password)
+        }
+    }
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) { // it: Task<AuthResult!>
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish ()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
